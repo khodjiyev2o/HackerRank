93081cba5fd8a6953a9caef8ca16958d973d4fab
Samandar <samandarkhodjiyev@gmail.com>
2022-08-01 13:58:32 +0900

    first challenge

---
+++ day#1.py
@@ -0,0 +1,43 @@
+##https://www.hackerrank.com/challenges/plus-minus/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    positive_nums = []
+    negative_nums = []
+    zeros = []
+    n = len(arr)
+    for num in arr :
+        if num >0:
+            positive_nums.append(num)
+        elif num<0:
+            negative_nums.append(num)
+        elif num==0:
+            zeros.append(num)
+        p_len = len(positive_nums)
+        n_len = len(negative_nums)
+        z_len = len(zeros)
+        coef_p= p_len/n
+        coef_n= n_len/n
+        coef_z= z_len/n
+    print(coef_p)
+    print(coef_n)
+    print(coef_z)   
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
