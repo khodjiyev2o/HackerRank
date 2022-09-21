0b7a34d81f4235a6817eb3230c41d221df80d216
Samandar <92217667+khodjiyev2o@users.noreply.github.com>
2022-09-21 14:21:42 +0900

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -337,3 +337,10 @@ for i in range(1,6+1):
 #     elif k < max_height :
 #         result = max_height - k 
 #     return result 
+
+
+
+def fib(n):
+    if n in [0,1]:
+        return n
+    return fib(n-1) + fib(n-2)
